e26c59a38566e4c902ac6e222415e6a5eb84acc1
Scrabsi <Scrabsilicious@gmail.com>
2025-05-14 17:28:37 -0500

    switching machines 5/14

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_word_count
+from stats import get_char_count
 
 def main():
         book_path = "books/frankenstein.txt"
         text = get_book_text(book_path)
         word_count = get_word_count(text)
+        char_count = get_char_count(text)
         print(f"{word_count} words found in the document")
+        print(char_count)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_word_count(text):
      words = text.split()
      word_count = len(words)
      return word_count
+
+def get_char_count(text):
+     char_count = {}
+     for char in text:
+          char = char.lower()
+          count = char_count.get(char, 0)
+          char_count[char] = count + 1
+     return char_count
+
+def get_sorted
